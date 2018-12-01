a62f85722a618b738b2b49799cb09c851c4410ad
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-11-30 21:45:37 -0800

    1.0.3: use tslint config file for excluding instead of command line; exclude all dist files as well as all node_modules files

--- cli/lint.sh
+++ cli/lint.sh
@@ -7,7 +7,7 @@ ${UTILITIES_DIRECTORY}cli/share.sh tsconfig.json
 ${UTILITIES_DIRECTORY}cli/share.sh tsconfig-common.json
 ${UTILITIES_DIRECTORY}cli/share.sh test/tsconfig.json
 
-tslint '**/*.ts{,x}' -e **/node_modules/** --fix -p tsconfig.json
+tslint '**/*.ts{,x}' --fix -p tsconfig.json
 tslint 'test/**/*.ts{,x}' --fix -p test/tsconfig.json
 
 ${UTILITIES_DIRECTORY}cli/unshare.sh tslint.json
