5a42306da67e3374e5dc03fb0f13183080a5d1d0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-04 15:54:20 -0800

    1.0.14: solve lint issue

--- cli/lint.sh
+++ cli/lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-tslint '**/*.ts{,x}' --fix -p tsconfig.json
-tslint 'test/**/*.ts{,x}' --fix -p tsconfig-node.json
+tslint 'src/**/*.ts{,x}' --fix -p ./tsconfig-jim.json
+tslint 'test/**/*.ts{,x}' --fix -p ./tsconfig-node.json
