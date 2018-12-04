0bf98457cf3fb0daf773a7d2cf0e698dcc03be7c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-04 15:55:45 -0800

    1.0.15: now with less jim

--- cli/lint.sh
+++ cli/lint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-tslint 'src/**/*.ts{,x}' --fix -p ./tsconfig-jim.json
+tslint 'src/**/*.ts{,x}' --fix -p ./tsconfig.json
 tslint 'test/**/*.ts{,x}' --fix -p ./tsconfig-node.json
