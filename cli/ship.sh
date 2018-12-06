d74c8d7165c4206af71572b2f08d0de7481aa9c7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-06 08:18:09 -0800

    v1.0.37: main rules

--- cli/ship.sh
+++ cli/ship.sh
@@ -3,7 +3,13 @@
 make test
 make lint
 VERSION=$(npm version patch)
+VERSIONED=$?
+if [[ ${VERSIONED} -ne 0 ]] ; then
+	printf "This project does not publish itself to npm. "
+fi
 git add .
 git commit -m "${VERSION}: ${MSG}"
 make push
-make publish
+if [[ ${VERSIONED} -ne 1 ]] ; then
+	make publish
+fi
