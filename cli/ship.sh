4224dda9b88961fd145c7b6d2b54b6ce8a640cdf
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-07 09:27:11 -0800

    v1.0.40: part spec -> note specs

--- cli/ship.sh
+++ cli/ship.sh
@@ -2,14 +2,19 @@
 
 make test
 make lint
+
 VERSION=$(npm version patch)
 VERSIONED=$?
 if [[ ${VERSIONED} -ne 0 ]] ; then
 	printf "This project does not publish itself to npm. "
 fi
+
+git submodule foreach git add .
+git submodule foreach git commit -m "${VERSION}: ${MSG}"
 git add .
 git commit -m "${VERSION}: ${MSG}"
 make push
+
 if [[ ${VERSIONED} -ne 1 ]] ; then
 	make publish
 fi
