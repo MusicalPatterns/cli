d5e00f577a43272a3d56c4410baf2117131b07f5
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-09 10:22:04 -0800

    v1.0.45: support overriding base Makefile commands (in particular build was important, the default returns there); also simplify Makefile

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
+BUILD_CMD="$1"
+DEFAULT_BUILD_CMD="tsc -p tsconfig-library.json"
+
 rm -rf dist/
 
-npm run build
+eval "${BUILD_CMD:=${DEFAULT_BUILD_CMD}}"
 BUILD_FAILED=$?
 
 if [[ ${BUILD_FAILED} != 0 ]] ; then
