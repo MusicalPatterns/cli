ae239510548b83adc0814c80254374edb691055e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-18 14:29:26 -0800

    v1.0.68: let us use this pattern for switch back to remote too then

--- bin/switch_back_to_remote.sh
+++ bin/switch_back_to_remote.sh
@@ -5,4 +5,9 @@ set -e
 rm -rf ../fake_npm_${REPO}
 
 npm uninstall @musical-patterns/${REPO}
-npm i -D @musical-patterns/${REPO}@latest
+if [[ $(npm list -dev -depth 0 | grep -m1 @musical-patterns/${REPO}) ]] ; then
+	npm i -D @musical-patterns/${REPO}@latest
+fi
+if [[ $(npm list -prod -depth 0 | grep -m1 @musical-patterns/${REPO}) ]] ; then
+	npm i @musical-patterns/${REPO}@latest
+fi
