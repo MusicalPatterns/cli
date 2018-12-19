d8f27ed8daf901e133550f9474f68de187925198
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-19 10:21:55 -0800

    gotta do it in this order if its going to work

--- bin/commit.sh
+++ bin/commit.sh
@@ -3,9 +3,9 @@
 set -e
 
 commit_recursively() {
+	git submodule foreach commit_recursively
 	git add .
 	git commit -m "${MSG}"
-	git submodule foreach commit_recursively
 }
 export -f commit_recursively
 commit_recursively
