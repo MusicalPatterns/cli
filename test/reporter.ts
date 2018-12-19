acc9a668a49013b4def59318413f83934011fac5
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-18 20:49:33 -0800

    simplify spec reporter

--- test/reporter.ts
+++ test/reporter.ts
@@ -1,17 +1,7 @@
 // tslint:disable:no-implicit-dependencies
-import { DisplayProcessor, SpecReporter } from 'jasmine-spec-reporter'
-import { CustomReporterResult } from 'jasmine-spec-reporter/built/spec-reporter'
-
-class ExperimentalProcessor extends DisplayProcessor {
-    // tslint:disable-next-line:class-methods-use-this
-    public displaySuite(suite: CustomReporterResult, log: string): string {
-        return `is this thing on? ${log}`
-    }
-}
+import { SpecReporter } from 'jasmine-spec-reporter'
 
 jasmine.getEnv()
     .clearReporters()
 jasmine.getEnv()
-    .addReporter(new SpecReporter({
-        customProcessors: [ ExperimentalProcessor ],
-    }))
+    .addReporter(new SpecReporter())
