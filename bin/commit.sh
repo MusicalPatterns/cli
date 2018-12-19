91554ee44752523f5b10c3f553efa752b057fdb1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-19 13:21:04 -0800

    v1.0.91: do not fail make commit if there is nothing to commit

--- bin/commit.sh
+++ bin/commit.sh
@@ -5,7 +5,7 @@ set -e
 commit_recursively() {
 	git submodule foreach commit_recursively
 	git add .
-	git commit -m "${MSG}"
+	git commit -m "${MSG}" || true
 }
 export -f commit_recursively
 commit_recursively
