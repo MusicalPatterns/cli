c3f8fb0e8b06e8018b6b5ec61e1ec0a6feb25bba
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-18 18:10:44 -0800

    v1.0.77: fix problem where switch back to remote would uninstall before it had the information about where to reinstall it

--- bin/switch_back_to_remote.sh
+++ bin/switch_back_to_remote.sh
@@ -4,10 +4,11 @@ set -e
 
 rm -rf ../fake_npm_${REPO}
 
-npm uninstall @musical-patterns/${REPO}
 if [[ $(npm list -dev -depth 0 | grep -m1 @musical-patterns/${REPO}) ]] ; then
+	npm uninstall @musical-patterns/${REPO}
 	npm i -D @musical-patterns/${REPO}@latest
 fi
 if [[ $(npm list -prod -depth 0 | grep -m1 @musical-patterns/${REPO}) ]] ; then
+	npm uninstall @musical-patterns/${REPO}
 	npm i @musical-patterns/${REPO}@latest
 fi
