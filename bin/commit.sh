3e48fb6d06e1b952fbafe3b92df8b83bd70bc3af
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-20 08:46:58 -0800

    commit nicely whether or not version is defined

--- bin/commit.sh
+++ bin/commit.sh
@@ -2,10 +2,16 @@
 
 set -e
 
+if [[ ${VERSION} == "" ]] ; then
+	VERSION_PREFIX=""
+else
+	VERSION_PREFIX="${VERSION}: "
+fi
+
 commit_recursively() {
 	git submodule foreach commit_recursively
 	git add .
-	git commit -m "${MSG}" || true
+	git commit -m "${VERSION_PREFIX}${MSG}" || true
 }
 export -f commit_recursively
 commit_recursively
