8b90ce39864750a2f9545022e1be686d8072b70d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-20 17:06:44 -0800

    process.env.PATTERN should be there for the lab's snapshot test... lets try another way

--- bin/test.sh
+++ bin/test.sh
@@ -12,7 +12,8 @@ JASMINE_BINARY=$([[ $? == 0 ]] && echo "${JASMINE_PATH}" || echo "${CLI_DIR}${JA
 
 tsc -p tsconfig-node.json
 if [[ $? == 0 ]] ; then
-	PATTERN=${PATTERN} ts-node -P tsconfig-node.json ${JASMINE_BINARY}
+	export PATTERN=${PATTERN}
+	ts-node -P tsconfig-node.json ${JASMINE_BINARY}
 else
 	exit 1
 fi
