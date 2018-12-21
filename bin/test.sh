6b2f4f62b1188b867b290cb319f3cf932682c6b9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-20 18:15:34 -0800

    attempt to make focused pattern test not compile other patterns... now it works

--- bin/test.sh
+++ bin/test.sh
@@ -12,7 +12,7 @@ JASMINE_PATH="node_modules/jasmine/bin/jasmine.js"
 test -f "$JASMINE_PATH"
 JASMINE_BINARY=$([[ $? == 0 ]] && echo "${JASMINE_PATH}" || echo "${CLI_DIR}${JASMINE_PATH}")
 
-tsc -p tsconfig-node.json
+tsc -p ${TSCONFIG}
 if [[ $? == 0 ]] ; then
 	ts-node -P ${TSCONFIG} ${JASMINE_BINARY}
 else
