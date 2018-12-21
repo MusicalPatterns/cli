9364a759a2191af040e9f201a489aa2115520bb9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-20 16:05:26 -0800

    support a custom build dir and keep the dist

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 
-BUILD_CMD="$1"
+BUILD_CMD="${1}"
 DEFAULT_BUILD_CMD="tsc -p tsconfig-library.json"
 
-rm -rf dist/
+CUSTOM_DIR="${2}"
+DIR=${CUSTOM_DIR:=.}
+
+rm -r ${DIR}/dist/* || true
+touch ${DIR}/dist/.gitkeep
 
 eval "${BUILD_CMD:=${DEFAULT_BUILD_CMD}}"
 BUILD_FAILED=$?
