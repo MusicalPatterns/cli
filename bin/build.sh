b4b8c59fd86533f939c1d96ee7894f681ed12fc9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-25 11:04:01 -0800

    hide error when build tries to remove stuff from dist but nothing is there (who cares)

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,7 @@ DEFAULT_BUILD_CMD="tsc -p tsconfig-library.json"
 CUSTOM_DIR="${2}"
 DIR=${CUSTOM_DIR:=.}
 
-rm -r ${DIR}/dist/* || true
+rm -r ${DIR}/dist/* > /dev/null 2>&1 || true
 touch ${DIR}/dist/.gitkeep
 
 eval "${BUILD_CMD:=${DEFAULT_BUILD_CMD}}"
