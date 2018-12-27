9c1e50c81e3bb73f35bf277bd3631df479b5bb17
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2018-12-27 12:04:39 -0800

    shorter npm command

--- bin/switch_back_to_remote.sh
+++ bin/switch_back_to_remote.sh
@@ -5,10 +5,10 @@ set -e
 rm -rf ../fake_npm_${REPO}
 
 if [[ $(npm list -dev -depth 0 | grep -m1 @musical-patterns/${REPO}) ]] ; then
-	npm uninstall @musical-patterns/${REPO}
+	npm rm @musical-patterns/${REPO}
 	npm i -D @musical-patterns/${REPO}@latest
 fi
 if [[ $(npm list -prod -depth 0 | grep -m1 @musical-patterns/${REPO}) ]] ; then
-	npm uninstall @musical-patterns/${REPO}
+	npm rm @musical-patterns/${REPO}
 	npm i @musical-patterns/${REPO}@latest
 fi
