c56d3013e20c464a67c590638b194bd2ca320af3
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-03 15:17:37 -0800

    simplify shared test config now that we dont try to run all tests together from lab

--- test/jasmine.js
+++ test/jasmine.js
@@ -4,10 +4,6 @@ module.exports = {
         'test/src/**/*.js',
         'test/src/**/*.tsx',
         'test/src/**/*.jsx',
-        'src/**/test/src/**/*.ts',
-        'src/**/test/src/**/*.js',
-        'src/**/test/src/**/*.tsx',
-        'src/**/test/src/**/*.jsx',
     ],
     helpers: [
         'test/setup.js',
