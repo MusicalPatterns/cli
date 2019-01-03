431319021dcfdb97aa0b9cf7401712cade760923
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-03 14:15:02 -0800

    resimplify test now that pattern-cli does test instead of lab

--- bin/test.sh
+++ bin/test.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "${PATTERN}" = "" -o "${PATTERN}" = "ALL" ] ; then
-	export JASMINE_CONFIG_PATH="${CLI_DIR}test/jasmine.js"
-	export TSCONFIG="tsconfig-node.json"
-else
-	export JASMINE_CONFIG_PATH="src/${PATTERN}/test/jasmine.js"
-	export TSCONFIG="src/${PATTERN}/tsconfig.json"
-fi
+export JASMINE_CONFIG_PATH="${CLI_DIR}test/jasmine.js"
+export TSCONFIG="tsconfig-node.json"
 
 JASMINE_PATH="node_modules/jasmine/bin/jasmine.js"
 test -f "$JASMINE_PATH"
