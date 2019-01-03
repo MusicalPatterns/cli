339d6ee1fe8dc6aa5f20041ae65e958c6964704a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-03 13:57:33 -0800

    lint no longer has to be complicated for patterns

--- bin/lint.sh
+++ bin/lint.sh
@@ -2,11 +2,5 @@
 
 set -e
 
-if [[ ${PATTERN} == "" || ${PATTERN} == "ALL" ]] ; then
-	DIR="."
-else
-	DIR="./src/${PATTERN}"
-fi
-
-tslint "${DIR}/src/**/*.ts{,x}" -e **/dist/** --fix -p ./tsconfig.json
-tslint "${DIR}/test/**/*.ts{,x}" --fix -p ./tsconfig-node.json
+tslint "src/**/*.ts{,x}" -e **/dist/** --fix -p ./tsconfig.json
+tslint "test/**/*.ts{,x}" --fix -p ./tsconfig-node.json
