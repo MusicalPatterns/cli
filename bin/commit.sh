e855a1a8bbeaa0728aaf440130a7d80107d4ade2
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 14:46:46 -0800

    1.0.185: do not rely on version patching for the version for the commit message, and always include it - and be quiet about it - and actually publish still oops! - and quiet wasnt the problem - and jesus i left push commented out in ship too - and trim whitespace

--- bin/commit.sh
+++ bin/commit.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g')
+VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
 commit_recursively() {
 	git submodule foreach commit_recursively || return
