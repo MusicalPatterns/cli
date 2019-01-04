7c047223c90d975df970bf57015c0508b66a7f86
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 11:20:41 -0800

    surface sharing individual files so that pattern can use it - but make it so that it can actually do it on postinstall - hopefually actually this time it works

--- bin/share_config.sh
+++ bin/share_config.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
 ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES="../../.."
+CLI_DIRECTORY="node_modules/@musical-patterns/cli/"
 
-FILE=tsconfig-common.json ./bin/share_file.sh
-FILE=tsconfig-library.json ./bin/share_file.sh
-FILE=tsconfig.json ./bin/share_file.sh
-FILE=tsconfig-node.json ./bin/share_file.sh
-FILE=tslint.json ./bin/share_file.sh
-FILE=test/tslint.json ./bin/share_file.sh
-FILE=test/mockDom.ts ./bin/share_file.sh
-FILE=test/reporter.ts ./bin/share_file.sh
-FILE=test/jasmine.js ./bin/share_file.sh
-FILE=Makefile ./bin/share_file.sh
-FILE=.gitattributes ./bin/share_file.sh
-FILE=.travis.yml ./bin/share_file.sh
-FILE=LICENSE ./bin/share_file.sh
+pushd ${ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES}
+	FILE=tsconfig-common.json ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=tsconfig-library.json ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=tsconfig.json ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=tsconfig-node.json ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=tslint.json ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=test/tslint.json ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=test/mockDom.ts ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=test/reporter.ts ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=test/jasmine.js ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=Makefile ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=.gitattributes ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=.travis.yml ${CLI_DIRECTORY}/bin/share_file.sh
+	FILE=LICENSE ${CLI_DIRECTORY}/bin/share_file.sh
+popd
 
 share_file_with_npm_struggles() {
 	FILE="$1"
