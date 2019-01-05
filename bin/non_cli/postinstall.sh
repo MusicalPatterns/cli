d43093ab0d3d55a75d104de72670325ab97ebf00
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 16:18:34 -0800

    1.0.200: share the post-install check too - in a way that can be sourced without mucking stuff up

--- bin/non_cli/postinstall.sh
+++ bin/non_cli/postinstall.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL="../../../"
-
-run_only_if_not_self_installing() {
-	CMD="$1"
-	if [[ -d "${CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL}node_modules" ]] ; then
-		eval "${CMD}"
-	fi
-}
-export -f run_only_if_not_self_installing
+. bin/non_cli/run_only_if_not_self_installing
 
 run_only_if_not_self_installing "sh ./bin/non_cli/share_config.sh"
---
+++ bin/non_cli/run_only_if_not_self_installing.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL="../../../"
+
+run_only_if_not_self_installing() {
+	CMD="$1"
+	if [[ -d "${CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL}node_modules" ]] ; then
+		eval "${CMD}"
+	fi
+}
+export -f run_only_if_not_self_installing
