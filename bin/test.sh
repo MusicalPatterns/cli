f1f95f6ea23c1f9a27b6dd5ec53e2df5329a3c87
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 16:58:30 -0800

    1.0.206: always use bash, not sh

--- bin/test.sh
+++ bin/test.sh
@@ -5,10 +5,10 @@ export TSCONFIG="tsconfig-node.json"
 
 JASMINE_PATH="node_modules/jasmine/bin/jasmine.js"
 test -f "$JASMINE_PATH"
-JASMINE_BINARY=$([ $? = 0 ] && echo "${JASMINE_PATH}" || echo "${CLI_DIR}${JASMINE_PATH}")
+JASMINE_BINARY=$([[ $? == 0 ]] && echo "${JASMINE_PATH}" || echo "${CLI_DIR}${JASMINE_PATH}")
 
 tsc -p ${TSCONFIG}
-if [ $? = 0 ] ; then
+if [[ $? == 0 ]] ; then
 	ts-node -P ${TSCONFIG} ${JASMINE_BINARY}
 else
 	exit 1
