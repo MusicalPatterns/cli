d1d24a0e84e693067852d2792c3bdb7a82c67168
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-05 15:41:32 -0800

    : main commit messages dont have janky versions

--- bin/commit.sh
+++ bin/commit.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+VERSION=$(cat package.json 2>/dev/null | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+if [[ ${VERSION} == "" ]] ; then
+	VERSION_PREFIX=""
+else
+	VERSION_PREFIX="${VERSION}: "
+fi
 
 commit_recursively() {
 	git submodule foreach commit_recursively || return
