e7e46e6c01c47b5ad9baf0c47298a2be4a72ebd4
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-04 16:03:31 -0800

    1.0.198: quiter pushd

--- bin/use_latest_local.sh
+++ bin/use_latest_local.sh
@@ -4,17 +4,17 @@ set -e
 
 rm -rf ../fake_npm_${REPO}
 
-pushd ../${REPO}/
+pushd ../${REPO}/ > /dev/null 2>&1
 	make build_dev
 	npm pack
 	mv *.tgz ..
-popd
+popd > /dev/null 2>&1
 
-pushd ..
+pushd .. > /dev/null 2>&1
 	tar -xvzf *.tgz
 	mv package fake_npm_${REPO}
 	rm *.tgz
-popd
+popd > /dev/null 2>&1
 
 if [[ $(npm list -dev -depth 0 | grep -m1 @musical-patterns/${REPO}) ]] ; then
 	npm i -D ../fake_npm_${REPO}
