d58a9507f5ddb66299903024d260074872367fba
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-05 13:02:23 -0800

    1.0.219: dont fetter me with your stderr, npm list

--- bin/use_latest_local.sh
+++ bin/use_latest_local.sh
@@ -16,9 +16,9 @@ pushd .. > /dev/null 2>&1
 	rm *.tgz
 popd > /dev/null 2>&1
 
-if [[ $(npm list -dev -depth 0 | grep -m1 @musical-patterns/${SERVICE}) ]] ; then
+if [[ $(npm list -dev -depth 0 2>/dev/null | grep -m1 @musical-patterns/${SERVICE}) ]] ; then
 	npm i -D ../fake_npm_${SERVICE}
 fi
-if [[ $(npm list -prod -depth 0 | grep -m1 @musical-patterns/${SERVICE}) ]] ; then
+if [[ $(npm list -prod -depth 0 2>/dev/null | grep -m1 @musical-patterns/${SERVICE}) ]] ; then
 	npm i ../fake_npm_${SERVICE}
 fi
