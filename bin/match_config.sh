0f0a4c06982db1912c33dcbb715d3183be1ee352
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-05 19:13:41 -0800

    1.0.232: ship will actually fail if match config fails

--- bin/match_config.sh
+++ bin/match_config.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
+set -e
+
 check_match() {
 	FILE="$1"
 
+	set +e
 	cmp -s "${FILE}" "node_modules/@musical-patterns/cli/${FILE}"
 	if [[ $? != 0 ]] ; then
 		echo "mismatch: ${FILE}"
+		set -e
 		return 1
 	fi
+	set -e
 }
 
 while read SHARED_FILE ; do
