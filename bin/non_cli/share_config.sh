314128afa366c5b7715ce0028c6abaf610536090
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-05 19:03:00 -0800

    1.0.229: DRY list of shared files

--- bin/non_cli/share_config.sh
+++ bin/non_cli/share_config.sh
@@ -2,28 +2,9 @@
 
 . bin/non_cli/share_file.sh
 
-share_file tsconfig-common.json
-share_file tsconfig-library.json
-share_file tsconfig.json
-share_file tsconfig-node.json
-share_file tslint.json
-share_file test/tslint.json
-share_file test/mockDom.ts
-share_file test/reporter.ts
-share_file test/jasmine.js
-share_file Makefile
-share_file .gitattributes
-share_file .travis.yml
-share_file LICENSE
-share_file .idea/codeStyles/codeStyleConfig.xml
-share_file .idea/codeStyles/Project.xml
-share_file .idea/inspectionProfiles/Project_Default.xml
-share_file .idea/jsLinters/tslint.xml
-share_file .idea/compiler.xml
-share_file .idea/encodings.xml
-share_file .idea/jsLibraryMappings.xml
-share_file .idea/misc.xml
-share_file .idea/watcherTasks.xml
+while read SHARED_FILE ; do
+	share_file "$SHARED_FILE"
+done < bin/non_cli/shared_files.txt
 
 share_file_with_npm_struggles .gitignore
 share_file_with_npm_struggles .npmignore
