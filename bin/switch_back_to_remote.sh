041d891ae76ddd2601d8166da1e68b798ce6029a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-05 18:12:19 -0800

    1.0.226: npm list error hidigin, msised some

--- bin/switch_back_to_remote.sh
+++ bin/switch_back_to_remote.sh
@@ -4,11 +4,11 @@ set -e
 
 rm -rf ../fake_npm_${SERVICE}
 
-if [[ $(npm list -dev -depth 0 | grep -m1 @musical-patterns/${SERVICE}) ]] ; then
+if [[ $(npm list -dev -depth 0 2>/dev/null | grep -m1 @musical-patterns/${SERVICE}) ]] ; then
 	npm rm @musical-patterns/${SERVICE}
 	npm i -D @musical-patterns/${SERVICE}@latest
 fi
-if [[ $(npm list -prod -depth 0 | grep -m1 @musical-patterns/${SERVICE}) ]] ; then
+if [[ $(npm list -prod -depth 0 2>/dev/null | grep -m1 @musical-patterns/${SERVICE}) ]] ; then
 	npm rm @musical-patterns/${SERVICE}
 	npm i @musical-patterns/${SERVICE}@latest
 fi
