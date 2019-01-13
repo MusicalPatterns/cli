b6728de090653a9ab2fbe0119683a680f8ce461b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-13 09:50:01 -0800

    1.0.260: more helpful failure message

--- bin/index.sh
+++ bin/index.sh
@@ -9,5 +9,5 @@ ARG="$2"
 if [[ -f ${CLI_DIR}bin/${CMD}.sh ]] ; then
 	bash ${CLI_DIR}bin/${CMD}.sh "$ARG"
 else
-	echo "'musical-patterns-cli ${CMD}' is not a defined CLI command."
+	echo "'musical-patterns-cli ${CMD}' is not a defined CLI command. Or you are using a musical-patterns-cli from a foreign PATH and it is not actually installed in your current node_modules."
 fi
