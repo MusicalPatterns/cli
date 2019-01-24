a7358a430c39e348a616a1904bc823dc39c382a8
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-24 13:49:00 -0800

    1.0.305: but dont screw it up

--- bin/non_cli/share_files.sh
+++ bin/non_cli/share_files.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-. ${CLI_DIR:=.}bin/non_cli/share_file.sh
+. ${CLI_DIR:=.}/bin/non_cli/share_file.sh
 
 share_files() {
 	while read SHARED_FILE ; do
 		share_file "$SHARED_FILE"
-	done < ${CLI_DIR:=.}bin/non_cli/shared_files.txt
+	done < ${CLI_DIR:=.}/bin/non_cli/shared_files.txt
 }
 export -f share_files
