fdd7c447d14517935cf45e40713768fcde664116
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-24 14:17:25 -0800

    1.0.306: gotta use the users share_files

--- bin/non_cli/share_files.sh
+++ bin/non_cli/share_files.sh
@@ -5,6 +5,6 @@
 share_files() {
 	while read SHARED_FILE ; do
 		share_file "$SHARED_FILE"
-	done < ${CLI_DIR:=.}/bin/non_cli/shared_files.txt
+	done < ./bin/non_cli/shared_files.txt
 }
 export -f share_files
