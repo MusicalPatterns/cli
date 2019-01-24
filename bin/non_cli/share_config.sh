a008c63d2c20237a702460ecc6013612dadf16c2
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-24 14:31:08 -0800

    1.0.307: simplify sharing even more

--- bin/non_cli/share_config.sh
+++ bin/non_cli/share_config.sh
@@ -4,5 +4,9 @@
 
 share_files
 
+while read OTHER_SHARED_FILE ; do
+	share_file "$OTHER_SHARED_FILE"
+done < ./bin/non_cli/other_shared_files.txt
+
 cp share/gitignore ../../../.gitignore
 cp share/npmignore ../../../.npmignore
--- bin/non_cli/share_files.sh
+++ bin/non_cli/share_files.sh
@@ -3,8 +3,10 @@
 . ${CLI_DIR:=.}/bin/non_cli/share_file.sh
 
 share_files() {
-	while read SHARED_FILE ; do
+	shopt -s globstar
+	for SHARED_FILE in ./share/**/*
+	do
 		share_file "$SHARED_FILE"
-	done < ./bin/non_cli/shared_files.txt
+	done
 }
 export -f share_files
