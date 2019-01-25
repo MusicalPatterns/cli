9f8b8f938ed244e37ff0ad7b88217d61cb3c0351
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-25 12:52:42 -0800

    1.0.342: scratch that it wasnt my fault

--- bin/sharing/postinstall.sh
+++ bin/sharing/postinstall.sh
@@ -10,7 +10,6 @@ share_files_and_special_treatment_shared_files() {
 
 	share_files
 }
-export -f share_files_and_special_treatment_shared_files
 
 CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL=../../../node_modules/@musical-patterns/cli
 if [[ -d "${CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL}" ]] ; then
