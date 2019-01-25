64ab72e3df0deadc4593b77e452049240d2d7e69
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-24 16:42:32 -0800

    1.0.326: consolidate share_config into postinstall for simplicity - i dunno do i need to export - maybe just dont eval

--- bin/non_cli/run_only_if_not_self_installing.sh
+++ bin/non_cli/run_only_if_not_self_installing.sh
@@ -3,9 +3,9 @@
 CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL="../../../"
 
 run_only_if_not_self_installing() {
-	CMD="$1"
+	CMD=$1
 	if [[ -d "${CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL}node_modules" ]] ; then
-		eval "${CMD}"
+		${CMD}
 	fi
 }
 export -f run_only_if_not_self_installing
