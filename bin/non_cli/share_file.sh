46637a0be148a9bf4a0c8e6ccbc2e8a46a53023f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-24 17:00:43 -0800

    1.0.331: consolidate share_config into postinstall for simplicity - i dunno do i need to export - maybe just dont eval - ok wtf arg - gross - hurts - yowza - ok grep failed

--- bin/non_cli/share_file.sh
+++ bin/non_cli/share_file.sh
@@ -5,8 +5,10 @@ ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES="../../../"
 make_dir_for_file() {
 	FILE="$1"
 	echo "ok making a dir which for some reason breaks now"
+	set +e
 	DIR=$(grep -Po '.*(?=\/)' <<< "${FILE}")
 	echo "and the dir is"${DIR}
+	set -e
 	if [[ ${DIR} != "" ]] ; then
 		echo "so we are trying to make this dir"
 		echo ${ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES}${DIR}
