001a58fc8b624e2ce6c320545028f4dc7d3bad02
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-24 19:26:19 -0800

    1.0.336: expose match checking for other sharing services to use

--- bin/match-config.sh
+++ bin/match-config.sh
@@ -2,24 +2,6 @@
 
 set -e
 
-check_match() {
-	FILE="$1"
+. ${CLI_DIR:=.}/bin/sharing/check_matches.sh
 
-	set +e
-	cmp -s "${FILE}" "node_modules/@musical-patterns/cli/share/${FILE}"
-	if [[ $? != 0 ]] ; then
-		echo "mismatch against standardized configuration: ${FILE}. Please re-run 'npm i @musical-patterns/cli'."
-		set -e
-		return 1
-	fi
-	set -e
-}
-
-shopt -s globstar
-shopt -s dotglob
-for SHARED_FILE in node_modules/@musical-patterns/cli/share/**/*
-do
-	if [[ -f "${SHARED_FILE}" && ${SHARED_FILE:41} != "gitignore" && ${SHARED_FILE:41} != "npmignore" ]]; then
-		check_match ${SHARED_FILE:41}
-	fi
-done
+check_matches
---
+++ bin/sharing/check_matches.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+check_match() {
+	FILE="$1"
+	SERVICE="$2"
+
+	set +e
+	cmp -s "${FILE}" "node_modules/@musical-patterns/${SERVICE}/share/${FILE}"
+	if [[ $? != 0 ]] ; then
+		echo "mismatch against standardized configuration: ${FILE}. Please re-run 'npm i @musical-patterns/${SERVICE}'."
+		set -e
+		return 1
+	fi
+	set -e
+}
+
+check_matches() {
+	SERVICE=${1:=cli}
+
+	shopt -s globstar
+	shopt -s dotglob
+	SHARED_DIR=node_modules/@musical-patterns/${SERVICE}/share/
+	SHARED_DIR_PATH_CHAR_LENGTH=${#SHARED_DIR}
+	for SHARED_FILE in ${SHARED_DIR}**/*
+	do
+		if [[ -f "${SHARED_FILE}" && ${SHARED_FILE:SHARED_DIR_PATH_CHAR_LENGTH} != "gitignore" && ${SHARED_FILE:SHARED_DIR_PATH_CHAR_LENGTH} != "npmignore" ]]; then
+			check_match ${SHARED_FILE:SHARED_DIR_PATH_CHAR_LENGTH} ${SERVICE}
+		fi
+	done
+}
+export -f check_matches
