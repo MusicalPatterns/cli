7623e438aeb62bef491fae7b3a0c3540e54a42c9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-28 13:09:05 -0800

    1.0.372: cli manages sharing for all services

--- bin/sharing/postinstall.sh
+++ bin/sharing/postinstall.sh
@@ -8,7 +8,10 @@ share_files_and_special_treatment_shared_files() {
 	mv bin/sharing/gitignore ../../../.gitignore
 	mv bin/sharing/npmignore ../../../.npmignore
 
-	share_files
+	SERVICES=("cli" "utilities" "performer" "compiler" "pattern" "snapshot" "playroom" "lab")
+	for i in "${!SERVICES[@]}" ; do
+		share_files "${SERVICES[i]}"
+	done
 }
 
 CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL=../../../node_modules/@musical-patterns/cli
--- bin/sharing/share_files.sh
+++ bin/sharing/share_files.sh
@@ -1,14 +1,45 @@
 #!/usr/bin/env bash
 
-. ${CLI_DIR:=.}/bin/sharing/share_file.sh
+ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES="../../../"
+
+ignore_file() {
+	FILE="$1"
+
+	if [[ ${FILE} == "LICENSE" || ${FILE} == ".travis.yml" ]] ; then
+		return 0
+	fi
+
+	grep -q -x -F "${FILE}" ${ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES}.gitignore || echo "${FILE}" >> ${ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES}.gitignore
+}
+
+make_dir_for_file() {
+	FILE="$1"
+	set +e
+	DIR=$(grep -Po '.*(?=\/)' <<< "${FILE}")
+	set -e
+	if [[ ${DIR} != "" ]] ; then
+		mkdir -p ${ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES}${DIR}
+	fi
+}
+
+share_file() {
+	FILE="$1"
+	make_dir_for_file ${FILE}
+	cp "share/${FILE}" ${ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES}${FILE}
+	ignore_file ${FILE}
+}
 
 share_files() {
+	SERVICE=${1:=cli}
+
 	shopt -s globstar
 	shopt -s dotglob
-	for SHARED_FILE in ./share/**/*
+	SHARED_DIR=node_modules/@musical-patterns/${SERVICE}/share/
+	SHARED_DIR_PATH_CHAR_LENGTH=${#SHARED_DIR}
+	for SHARED_FILE in ${SHARED_DIR}/**/*
 	do
 	    if [[ -f "${SHARED_FILE}" ]]; then
-			share_file ${SHARED_FILE:8}
+			share_file ${SHARED_FILE:SHARED_DIR_PATH_CHAR_LENGTH}
 		fi
 	done
 }
