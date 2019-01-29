c6b77ebb7a26fe6db592082b144a51c2d47e68a2
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-28 16:09:52 -0800

    1.0.379: whole new world - a fantastic point of view - i dont care where to go - or how to grow

--- bin/sharing/share_files.sh
+++ bin/sharing/share_files.sh
@@ -36,11 +36,11 @@ export -f share_file
 share_files() {
 	SERVICE=${1:=cli}
 
-	SHARED_DIR=node_modules/@musical-patterns/${SERVICE}/share/
+	SHARED_DIR=${ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES}node_modules/@musical-patterns/${SERVICE}/share/
 	SHARED_DIR_PATH_CHAR_LENGTH=${#SHARED_DIR}
 
-	if [[ -d ${ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES}${SHARED_DIR} ]] ; then
-		find ${ESCAPE_CLI_DIRECTORY_IN_PARENTS_NODE_MODULES}${SHARED_DIR} -type f -exec bash -c 'share_file "$0" "$1"' ${SHARED_DIR_PATH_CHAR_LENGTH} {} \;
+	if [[ -d ${SHARED_DIR} ]] ; then
+		find ${SHARED_DIR} -type f -exec bash -c 'share_file "$0" "$1"' ${SHARED_DIR_PATH_CHAR_LENGTH} {} \;
 	fi
 }
 export -f share_files
