ae55c2d01edee54670388db4c98e19cddb6c437c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-31 11:15:49 -0800

    1.0.400: include integration testing dependenceis, and benchmark the time to build the code fro test

--- bin/test.sh
+++ bin/test.sh
@@ -7,7 +7,8 @@ JASMINE_PATH="node_modules/jasmine/bin/jasmine.js"
 test -f "$JASMINE_PATH"
 JASMINE_BINARY=$([[ $? == 0 ]] && echo "${JASMINE_PATH}" || echo "${CLI_DIR}${JASMINE_PATH}")
 
-tsc -p ${TSCONFIG}
+echo "Building the source code took:"
+time tsc -p ${TSCONFIG}
 if [[ $? == 0 ]] ; then
 	NODE_ENV=test ts-node -P ${TSCONFIG} ${JASMINE_BINARY}
 else
