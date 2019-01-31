557af7c5b467db034cdc8ec402ea0035cba04535
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-31 15:49:48 -0800

    correct word

--- bin/test.sh
+++ bin/test.sh
@@ -9,7 +9,7 @@ JASMINE_BINARY=$([[ $? == 0 ]] && echo "${JASMINE_PATH}" || echo "${CLI_DIR}${JA
 
 TIMEFORMAT=%R
 TIME_TO_BUILD_SRC=$( { time tsc -p ${TSCONFIG} > /dev/null 2>&1; } 2>&1 )
-echo "Building the source code for tests took "${TIME_TO_BUILD_SRC}" seconds."
+echo "Compiling the source code for tests took "${TIME_TO_BUILD_SRC}" seconds."
 if [[ $? == 0 ]] ; then
 	NODE_ENV=test ts-node -P ${TSCONFIG} ${JASMINE_BINARY}
 else
