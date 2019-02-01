18a0c7efaf3fa5ed97b3d69c15174daf5492bc7e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-01 09:26:23 -0800

    1.0.414: run test on a separate port

--- share/webpack.port.js
+++ share/webpack.port.js
@@ -3,5 +3,5 @@ const path = require('path')
 const repo = __dirname.split(path.sep).pop()
 
 module.exports = {
-    port: repo === 'performer' ? 8081 : 8080,
+    port: repo === 'performer' ? 8081 : process.env.NODE_ENV === 'test' ? 8082 : 8080,
 }
