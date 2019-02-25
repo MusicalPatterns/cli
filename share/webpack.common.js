98803b76160e08c0716c79d6f0bcd3748ec6151f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-24 16:23:13 -0800

    1.0.440: augment ts-loader to not crash as much

--- share/webpack.common.js
+++ share/webpack.common.js
@@ -1,3 +1,4 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const { DefinePlugin } = require('webpack')
 
 module.exports = {
@@ -7,6 +8,9 @@ module.exports = {
                 test: /\.tsx?$/,
                 loader: 'ts-loader',
                 exclude: /test\//,
+                options: {
+                    transpileOnly: true
+                }
             },
         ],
     },
@@ -17,5 +21,6 @@ module.exports = {
         new DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         }),
+        new ForkTsCheckerWebpackPlugin(),
     ],
 }
