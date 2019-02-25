8ef720cea9c930fd15ccc11a1d57e3137861adfa
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-24 16:12:11 -0800

    1.0.439: dont think you can give args to index directly anyway

--- bin/index.sh
+++ bin/index.sh
@@ -4,10 +4,9 @@ export CLI_DIR="./node_modules/@musical-patterns/cli/"
 export PATH=${PATH}:./node_modules/.bin/:${CLI_DIR}node_modules/.bin/
 
 CMD="$1"
-ARG="$2"
 
 if [[ -f ${CLI_DIR}bin/${CMD}.sh ]] ; then
-	bash ${CLI_DIR}bin/${CMD}.sh "$ARG"
+	bash ${CLI_DIR}bin/${CMD}.sh
 else
 	echo "'musical-patterns-cli ${CMD}' is not a defined CLI command. Or you are using a musical-patterns-cli from a foreign PATH and it is not actually installed in your current node_modules."
 fi
