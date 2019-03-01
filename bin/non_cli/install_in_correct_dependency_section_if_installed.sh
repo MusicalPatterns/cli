92ec869a1320a8b292b1aee8ab56ed63591d7f02
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-28 18:15:50 -0800

    1.0.457: make update should not match partial package names

--- bin/non_cli/install_in_correct_dependency_section_if_installed.sh
+++ bin/non_cli/install_in_correct_dependency_section_if_installed.sh
@@ -4,11 +4,11 @@ install_in_correct_dependency_section_if_installed() {
 	DEPENDENCY="$1"
 
 	DEV_DEPS_LIST=${DEV_DEPS_LIST:=$(npm list -dev -depth 0 2>/dev/null)}
-	if [[ $(echo ${DEV_DEPS_LIST} | grep -m1 "\-\-\ @musical-patterns/${PACKAGE}") ]] ; then
+	if [[ $(echo ${DEV_DEPS_LIST} | grep -m1 "\-\-\ @musical-patterns/${PACKAGE}@") ]] ; then
 		npm i -D "${DEPENDENCY}"
 	fi
 	PROD_DEPS_LIST=${PROD_DEPS_LIST:=$(npm list -prod -depth 0 2>/dev/null)}
-	if [[ $(echo ${PROD_DEPS_LIST} | grep -m1 "\-\-\ @musical-patterns/${PACKAGE}") ]] ; then
+	if [[ $(echo ${PROD_DEPS_LIST} | grep -m1 "\-\-\ @musical-patterns/${PACKAGE}@") ]] ; then
 		npm i -P "${DEPENDENCY}"
 	fi
 }
