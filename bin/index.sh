0088ccba655d894da6739dd2f9a462b0565b6ed7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-03-15 19:40:44 -0700

    1.0.493: improved index fail msg

--- bin/index.sh
+++ bin/index.sh
@@ -8,5 +8,5 @@ CMD="$1"
 if [[ -f ${CLI_DIR}bin/${CMD}.sh ]] ; then
 	bash ${CLI_DIR}bin/${CMD}.sh
 else
-	echo "'musical-patterns-cli ${CMD}' is not a defined CLI command. Or you are using a musical-patterns-cli from a foreign PATH and it is not actually installed in your current node_modules."
+	echo "'musical-patterns-cli ${CMD}' is not a defined CLI command. Or you are using a musical-patterns-cli from a foreign PATH and it is not actually installed in your current node_modules. Try running 'which musical-patterns-cli' to see what's up."
 fi
