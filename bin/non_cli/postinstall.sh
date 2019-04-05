d5a8cdb93cb36e32413c6aa945600c887c9d75d6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-05 15:28:41 -0700

    1.0.537: i dont care if this is 2 diff ways for now if it works it works - but i do have to not lose the mv to cp thing - and maybe this dsoiadgoitg - oh ok i think i just made a dumb mistake but as long as we are here lets see if we can resolve the 2 diff ways - wooot great only 1 way in the end, and clean up echo logging

--- bin/non_cli/postinstall.sh
+++ bin/non_cli/postinstall.sh
@@ -4,5 +4,5 @@ set -e
 
 CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL=../../../node_modules/@musical-patterns/cli
 if [[ -d "${CHECK_TO_MAKE_SURE_I_AM_RUNNING_AS_PART_OF_ANOTHER_MODULE_S_INSTALL}" ]] ; then
-	FROM_CLI=true bash bin/sharing/share_config.sh
+	bash bin/sharing/share_config.sh
 fi
