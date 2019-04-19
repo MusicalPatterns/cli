cf204b5e48039846d751c0b95d788ee7a517bad1
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-19 15:42:56 -0700

    1.0.553: only patch version if the build succeeds

--- bin/publish.sh
+++ bin/publish.sh
@@ -5,8 +5,8 @@ set -e
 . ${CLI_DIR:=./}bin/non_cli/run_only_if_not_clean.sh
 
 publish() {
-	npm version patch > /dev/null 2>&1 || return
 	make build || return
+	npm version patch > /dev/null 2>&1 || return
 	npm publish --access public
 }
 
