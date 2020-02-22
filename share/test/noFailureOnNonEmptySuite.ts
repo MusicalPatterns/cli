37339828c5bcb4a79ae9cfe6ace0db496bc6db59
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-02-21 21:07:18 -0800

    1.0.572: another shared test file linted

--- share/test/noFailureOnNonEmptySuite.ts
+++ share/test/noFailureOnNonEmptySuite.ts
@@ -1,5 +1,5 @@
-describe('when jasmine has no way to configure its exit code to not be error even when it prints SUCCESS on empty test suite', () => {
-    it('has at least one test', () => {
+describe('when jasmine has no way to configure its exit code to not be error even when it prints SUCCESS on empty test suite', (): void => {
+    it('has at least one test', (): void => {
         expect(true)
             .toBeTruthy()
     })
