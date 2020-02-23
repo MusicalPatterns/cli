aa53fb05ab85b2ca94ea11d805a1a2d255a6012d
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2020-02-23 14:50:18 -0800

    1.0.574: spaceport

--- share/bin/port.js
+++ share/bin/port.js
@@ -3,7 +3,7 @@ const { readFileSync } = require('fs')
 
 const pathParts = __dirname.split(sep)
 const STEP_UP_OUT_OF_BIN_INTO_ROOT_OF_REPO_I_AM_COPIED_INTO = 1
-const currentRepo = pathParts[ pathParts.length - 1 - STEP_UP_OUT_OF_BIN_INTO_ROOT_OF_REPO_I_AM_COPIED_INTO]
+const currentRepo = pathParts[ pathParts.length - 1 - STEP_UP_OUT_OF_BIN_INTO_ROOT_OF_REPO_I_AM_COPIED_INTO ]
 
 let port
 readFileSync('ports.txt', 'utf-8').split(/\r?\n/).forEach(line => {
