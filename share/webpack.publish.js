693b2759e37ac181237a01fb6ce29e9578ca21e7
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2022-09-26 16:08:19 -0700

    1.0.579:

--- share/webpack.publish.js
+++ share/webpack.publish.js
@@ -2,6 +2,6 @@ const self = require('./webpack.self')
 const common = require('./webpack.common')
 const prod = require('./webpack.prod')
 const library = require('./webpack.library')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 
 module.exports = merge(self, common, prod, library)
