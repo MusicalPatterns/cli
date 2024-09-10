f1442e330bea83c4383cbe676c66c3fde539c1a4
Douglas Blumeyer <douglas@mechanical-orchard.com>
2024-09-09 21:28:59 -0700

    remove lint

--- bin/lint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# tslint is deprecated. some day, switch to eslint and typescript-eslint.
-
-# tslint "src/**/*.ts{,x}" -e **/dist/** --fix -p ./tsconfig.json
-# tslint "test/**/*.ts{,x}" --fix -p ./tsconfig.node.json
